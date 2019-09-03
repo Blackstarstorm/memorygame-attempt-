b2f41d0693a7db014cdd78149e878b3fdf58a973
Blackstarstorm <53792352+Blackstarstorm@users.noreply.github.com>
2019-09-03 16:42:43 -0400

    Add files via upload

---
+++ wdi-fundamentals-memorygame/main.js
@@ -0,0 +1,57 @@
+const cards[ { rank: "queen",
+suit: "hearts",
+image:"images/queen-of-hearts.png"
+}, 
+{ 
+	rank:"queen",
+suit: "diamonds",
+image: "images/queen-of-diamonds.png"
+}, 
+{
+	rank:"king",
+suit: "hearts",
+image: "images/king-of-hearts.png"
+},
+
+{
+	rank:"king",
+suit:"diamonds",
+images:"images/king-of-diamonds.png" 
+} 
+];
+
+const cardsInPlay[""];
+
+function checkForMatch() { 
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+		else {
+			console.log("Sorry, try agian");
+		}
+	}
+};
+
+function flipCard() {
+	{
+		if {
+this.getAttribute('click', 'data-id');
+document.setAttribute('src', 'cardImage');
+console.log(cards[cardId].cardImage);
+}
+
+};
+
+ cardsInPlay.push(cards[cardId].rank);{
+ 	console.log("User flipped queen");
+ 	console.log("User flipped king");
+ };
+
+function createBoard() {
+	for (var i = 0; i < createBoard.length;i++){
+		var newListItem = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', 'flipCard');
+		cardElement.getElementsByTagName('game-board')[0].appendChild('newListItem');
+		}
+	};
